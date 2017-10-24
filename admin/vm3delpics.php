d78fe608c8e87b9ff70eead84632fc3d75139215
Boris <borro@inbox.ru>
2017-10-24 12:34:34 +0200

    Add files via upload

--- admin/vm3delpics.php
+++ admin/vm3delpics.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * @author     Boris Garkoun <borro@inbox.ru>
+ * @date       24.10.17
+ *
+ * @copyright  Copyright (C) 2017 - 2017 https://forum.joomla.org/memberlist.php?mode=viewprofile&u=574731 . All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE
+ */
+
 defined("_JEXEC") or die();
 
 $controller = JControllerLegacy::getInstance('Vm3delpics');
